d91af275dca74ee14e12e550a06e136bccca4398
resoaga <deine@email.com>
2025-11-23 15:08:40 +0100

    vorbereitung JS.

--- eingabe.js
+++ eingabe.js
@@ -1,10 +1,16 @@
 /* es braucht ID um es in JS aufrufen zu können zb. <td id="mehl-menge">....</td>
 
+eine universelle losung, ein JS der die einheit und menge bezieht
+let* menge
+let* einheit
+dann eine funktion die denn input feld bzw zum button gebunden und ausgelost wird
 
+function* button zb. berechneneumenge*
 
+Die function ist eine matematisches einfache multipilizeren aufgabe
 
+menge x portion = function
 
-
-
+achten auf koma nachstellen.
 
 */
